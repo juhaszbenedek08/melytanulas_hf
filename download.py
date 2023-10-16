168a595811c9cdf2d0135544508cfe9d165b83dc
tadam <example@example.com>
2023-10-16 15:01:36 +0200

    valami kód

--- download.py
+++ download.py
@@ -1,5 +1,46 @@
 import subprocess
 from pathlib import Path
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+import pandas as pd
+
+
+class ColonDataset(Dataset):
+
+    def __init__(self):
+        self.raw_dir = Path('')
+        self.all_scans = [
+            [
+                [
+                    (
+                        int(case_dir.name.removeprefix('case_')),
+                        int(day_dir.name.removeprefix(f'{case_dir.name}_day')),
+                        int(scan_path.name.split('_')[1]),
+                        scan_path
+                    )
+                    for scan_path in (day_dir / 'scans').iterdir()
+                ]
+                for day_dir in case_dir.iterdir()
+            ]
+            for case_dir in Path(self.raw_dir).iterdir()
+        ]
+        self.annots = pd.read_csv()
+
+    def __len__(self):
+        return len(self.all_scans)
+
+    def __getitem__(self, item):
+        scan = self.all_scans[item]
+        fname = scan[3]
+        img = read_image(fname)
+        annotname = f"case{scan[0]}_day{scan[1]}_slice_{scan[2]}"
+        annots = self.annots.where(self.annots['id'] == annotname)
+        lb_annot = annots['segmentation'].where(annots['class'] == 'large_bowel')[0]
+        sb_annot = annots['segmentation'].where(annots['class'] == 'small_bowel')[0]
+        st_annot = annots['segmentation'].where(annots['class'] == 'stomach')[0]
+#         todo make masks from annots, normalize img
+
+        return img, lb_annot, sb_annot, st_annot
 
 
 def main():
@@ -36,4 +77,3 @@ def main():
     # Create splits
 
     # Create dataset & dataloader
-
